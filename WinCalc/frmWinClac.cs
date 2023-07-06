84d6def266d2be5bf56267c30c5a86b35cf3dd0d
Suphawhadee520Gorgeous <suphawhadee.monthathat2468@gmail.com>
2023-07-06 11:51:50 +0700

    เกือบ100%

--- WinCalc/frmWinClac.cs
+++ WinCalc/frmWinClac.cs
@@ -5,23 +5,52 @@ namespace WinCalc
 {
     public partial class frmWinClac : Form
     {
-        private string number1 = "", number2 = "",answer = "";
+        private string number1 = "", number2 = "", answer = "";
+        private bool dotStatus = false;
+        private char symbol = '0';
 
         private void AddToDisplay(string numberSymbol)
         {
-            if(this.txtDisplay.Text == "0")
-            {
-                //this.txtDisplay.Text = numberSymbol;
+            if (numberSymbol == ".")
+                this.dotStatus = true;
 
-                if(numberSymbol == ".")
-                {
-                    this.txtDisplay.Text += numberSymbol;
-                }
-            }
-            else
+            if (this.txtDisplay.Text == "0" && numberSymbol == ".")
             {
                 this.txtDisplay.Text += numberSymbol;
             }
+            else if (this.txtDisplay.Text == "0")
+            {
+                this.txtDisplay.Text = numberSymbol;
+            }
+            else if(this.txtDisplay.Text != "0")
+            {
+                this.txtDisplay.Text += numberSymbol;
+            }
+           
+        }
+
+        private string calculating(string num1, string num2, char _symbol)
+        {
+            double numFirst = double.Parse(num1);
+            double numSecond = double.Parse(num2);
+            double numAnswer = 0.0f;
+
+            switch (this.symbol) 
+            {
+            case '+': numAnswer = numFirst + numSecond; break;
+            case '-': numAnswer = numFirst - numSecond; break;
+            case '*': numAnswer = numFirst * numSecond; break;
+            case '/': numAnswer = numFirst / numSecond; break;
+            }
+            return numAnswer.ToString();
+        }
+
+        private void setNumber1(char _symbol)
+        {
+            this.symbol = _symbol;
+            this.number1 = this.txtDisplay.Text;
+            this.txtDisplay.Text = "0";
+            this.dotStatus = false;
         }
         public frmWinClac()
         {
@@ -35,40 +64,43 @@ namespace WinCalc
 
         private void btnDot_Click(object sender, EventArgs e)
         {
-            this.AddToDisplay(".");
+            if (this.dotStatus)
+                this.AddToDisplay(".");
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-
+            this.number2 = this.txtDisplay.Text;
+            this.txtDisplay.Text =
+            this.calculating(this.number1, number2, this.symbol);
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-
+            this.setNumber1('/');
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-
+            this.setNumber1('*');
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-
+            this.setNumber1('-');
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            this.setNumber1('+');
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             this.number1 = this.number2 = this.answer = "0";
             this.txtDisplay.Text = "0";
-            this.symbol.Text = "0";
-            this.doStatus  = false;
+            this.symbol = '0';
+            this.dotStatus  = false;
         }
 
         private void btnBack_Click(object sender, EventArgs e)
